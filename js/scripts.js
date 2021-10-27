7758b171582846222f8d57906e840a1f24a42272
Zoe Weinstein <mxzweinstein@gmail.com>
2021-10-26 21:17:29 -0600

    add first 3 functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
-  const sentence = prompt("Enter a sentence");
+function getSentence(){
+  const sentence = prompt("Enter a sentence: ");
+  return sentence;
+}
 
+function capitalize(sentence){
+  const senLength = sentence.length;
+  const newSentence = sentence.charAt(0).toUpperCase() + 
+                      sentence.slice(1,senLength -2) + 
+                      sentence.charAt(senLength-1).toUpperCase();
+  return newSentence;
+
+}
+function printSentence(sentence){
   console.log(sentence);
-});
+}
+printSentence(capitalize(getSentence()));
